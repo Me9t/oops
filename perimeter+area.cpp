6253e39962d0381d9a5d8137384fccab5044f666
Me9t <meetmavjivaviya2@gmail.com>
2025-04-25 23:37:55 +0530

    Update perimeter+area.cpp

--- perimeter+area.cpp
+++ perimeter+area.cpp
@@ -1,36 +1,40 @@
 #include<iostream>
 using namespace std;
-class Rectangle{
-    private:
-        float breath;
-        float length;
-    
-    public:
-    void getInput(){
-        cout<<"Enter A Length: ";
-        cin>>length;
-        cout<<"Enter a breath: ";
-        cin>>breath;
+
+class Rectangle {
+private:
+    float breadth;
+    float length;
+
+public:
+    // Function to get input from user
+    void getInput() {
+        cout << "Enter the length: ";
+        cin >> length;
+        cout << "Enter the breadth: ";
+        cin >> breadth;
     }
-    
-    float calculateArea(){
-        return length*breath;
+
+    // Function to calculate area
+    float calculateArea() {
+        return length * breadth;
     }
-    
-    float calculatePerimeter(){
-        return 2 * (length+breath);
+
+    // Function to calculate perimeter
+    float calculatePerimeter() {
+        return 2 * (length + breadth);
     }
-    
-    void displayValue(){
-        cout<<"Area of the rectangle is " <<calculateArea()<<endl;
-        cout<<"Perimeter of the rectangle is "<<calculatePerimeter()<<endl;
+
+    // Function to display results
+    void displayValue() {
+        cout << "Area of the rectangle is: " << calculateArea() << endl;
+        cout << "Perimeter of the rectangle is: " << calculatePerimeter() << endl;
     }
 };
 
-
-
-int main(){
+int main() {
     Rectangle rect;
     rect.getInput();
     rect.displayValue();
+    return 0;
 }
